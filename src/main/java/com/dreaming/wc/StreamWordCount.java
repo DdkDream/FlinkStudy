8778e34f951b6b8070923bd0f985ebefe2c370ac
zrm <643910751@qq.com>
2022-10-17 14:47:25 +0800

    简单上手（二）流处理WordCount

---
+++ src/main/java/com/dreaming/wc/StreamWordCount.java
@@ -0,0 +1,33 @@
+package com.dreaming.wc;
+
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class StreamWordCount {
+    public static void main(String[] args) throws Exception {
+        // 创建流处理执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        // 从文件中读取数据
+        String inputPath = "/Volumes/Dreaming/Code/Java/FlinkStudy/src/main/resources/hello.txt";
+        DataStream<String> inputDataStream = env.readTextFile(inputPath);
+
+        // 基于数据流进行转换计算
+        SingleOutputStreamOperator<Tuple2<String, Integer>> resultStream = inputDataStream.flatMap(new WordCount.MyFlatMapper())
+                .keyBy(0)
+                .sum(1);
+
+
+
+        resultStream.print();
+
+
+        // 执行任务
+        env.execute();
+    }
+}
