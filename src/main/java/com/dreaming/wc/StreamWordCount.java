c86b9bbc5dd238a4fa666ad1134cd1c388c31d44
zrm <643910751@qq.com>
2022-10-17 15:06:44 +0800

    简单上手（二）流式数据源测试

--- src/main/java/com/dreaming/wc/StreamWordCount.java
+++ src/main/java/com/dreaming/wc/StreamWordCount.java
@@ -2,6 +2,7 @@ package com.dreaming.wc;
 
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
@@ -13,9 +14,19 @@ public class StreamWordCount {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
-        // 从文件中读取数据
-        String inputPath = "/Volumes/Dreaming/Code/Java/FlinkStudy/src/main/resources/hello.txt";
-        DataStream<String> inputDataStream = env.readTextFile(inputPath);
+//        // 从文件中读取数据
+//        String inputPath = "/Volumes/Dreaming/Code/Java/FlinkStudy/src/main/resources/hello.txt";
+//        DataStream<String> inputDataStream = env.readTextFile(inputPath);
+
+        // 用parameter tool工具从程序启动参数中提取配置项
+        ParameterTool parameterTool = ParameterTool.fromArgs(args);
+        String host = parameterTool.get("host");
+        int port = parameterTool.getInt("port");
+
+
+        // 从socket文本流读取数据
+        DataStream<String> inputDataStream = env.socketTextStream(host, port);
+
 
         // 基于数据流进行转换计算
         SingleOutputStreamOperator<Tuple2<String, Integer>> resultStream = inputDataStream.flatMap(new WordCount.MyFlatMapper())
