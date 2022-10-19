2b6e8c8c1fc62fc4a9b45d4224e27146db9090fc
zrm <643910751@qq.com>
2022-10-19 14:55:17 +0800

    Flink快速上手

--- src/main/java/com/dreaming/wc/BoundedStreamWordCount.java
+++ src/main/java/com/dreaming/wc/BoundedStreamWordCount.java
@@ -2,28 +2,22 @@ package com.dreaming.wc;
 
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
 
-public class StreamWordCount {
+public class BoundedStreamWordCount {
     public static void main(String[] args) throws Exception {
-        // 1、创建流失执行环境
+        // 1、创建流失的执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        // 从参数中提取主机名和端口号
-        ParameterTool parameterTool = ParameterTool.fromArgs(args);
-        String hostname = parameterTool.get("host");
-        Integer port = parameterTool.getInt("port");
+        // 2、读取文件
+        DataStreamSource<String> lineDataStreamSource = env.readTextFile("input/word.txt");
 
-
-        // 2、读取文件流
-        DataStreamSource<String> lineDataStream = env.socketTextStream(hostname, port);
-
-        SingleOutputStreamOperator<Tuple2<String, Long>> wordAndOneTuple = lineDataStream.flatMap((String line, Collector<Tuple2<String, Long>> out) -> {
+        // 3、转换计算
+        SingleOutputStreamOperator<Tuple2<String, Long>> wordAndOneTuple = lineDataStreamSource.flatMap((String line, Collector<Tuple2<String, Long>> out) -> {
                     String[] words = line.split(" ");
                     for (String word : words) {
                         out.collect(Tuple2.of(word, 1L));
@@ -31,16 +25,16 @@ public class StreamWordCount {
                 })
                 .returns(Types.TUPLE(Types.STRING, Types.LONG));
 
-        // 4、分组
+        // 4、分组操作
         KeyedStream<Tuple2<String, Long>, String> wordAndOneKeyedStream = wordAndOneTuple.keyBy(data -> data.f0);
 
         // 5、求和
         SingleOutputStreamOperator<Tuple2<String, Long>> sum = wordAndOneKeyedStream.sum(1);
 
-        // 6、输出
+        // 6、打印
         sum.print();
 
-        // 7、执行
-        env.execute();
+        // 7、启动执行
+         env.execute();
     }
 }
