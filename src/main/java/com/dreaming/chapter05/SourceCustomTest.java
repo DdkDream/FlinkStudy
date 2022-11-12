8ecb0c11c810afaa320fe2fc732623c6abeffb3f
zrm <643910751@qq.com>
2022-11-12 19:21:14 +0800

    DataStream API(基础篇四)

--- src/main/java/com/dreaming/chapter05/SourceCustomTest.java
+++ src/main/java/com/dreaming/chapter05/SourceCustomTest.java
@@ -2,17 +2,39 @@ package com.dreaming.chapter05;
 
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction;
+
+import java.util.Random;
 
 public class SourceCustomTest {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
-        DataStreamSource<Event> customStream = env.addSource(new ClickSource());
+        //DataStreamSource<Event> customStream = env.addSource(new ClickSource());
+        DataStreamSource<Integer> customStream = env.addSource(new ParallelCustomSource()).setParallelism(2);
 
         customStream.print();
 
         env.execute();
 
+    }
+
+    // 实现自定义的并行SourceFunction
+    public static class ParallelCustomSource implements ParallelSourceFunction<Integer>{
+        private boolean running = true;
+        private Random random = new Random();
+
+        @Override
+        public void run(SourceContext<Integer> sourceContext) throws Exception {
+            while(running){
+                sourceContext.collect(random.nextInt());
+            }
+        }
+
+        @Override
+        public void cancel() {
+            running = false;
+        }
     }
 }
